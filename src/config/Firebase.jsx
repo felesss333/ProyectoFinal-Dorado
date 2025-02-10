10283574342db1c2ff7c0e3ca0338a5214fb3824
Felipe Dorado <72577022+felesss333@users.noreply.github.com>
2025-02-10 11:52:31 -0300

    Update Firebase.jsx

--- src/config/Firebase.jsx
+++ src/config/Firebase.jsx
@@ -8,12 +8,12 @@ import { productos }  from "../data/asynMock/";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-VITE_FIREBASE_APIKEY=AIzaSyDEzWR1f412J_y0L0z3mGMPm5VV6o_a-w0
-VITE_FIREBASE_AUTHDOMAIN=tiendaquebble.firebaseapp.com
-VITE_FIREBASE_PROJECTID=tiendaquebble
-VITE_FIREBASE_STORAGEBUCKET=tiendaquebble.appspot.com
-VITE_FIREBASE_MESSAGINGSENDERID=575435622453
-VITE_FIREBASE_APPID=1:575435622453:web:b8cdaf438c658b38f5097c
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID
 };
 
 console.log('config se conectó')
