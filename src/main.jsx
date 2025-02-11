ab31fc12eeea7727467c5cd40ba8fdce9ac40719
Felipe Dorado <72577022+felesss333@users.noreply.github.com>
2025-02-11 14:06:08 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import { initializeApp } from "firebase/app";
-import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { addDoc, collection, getDocs, getFirestore } from "firebase/firestore";
 import { productos } from './data/asynMock.jsx';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain:import.meta.env.VITE_FIREBASE_AUTHDOMAIN ,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
   appId: import.meta.env.VITE_FIREBASE_APPID
 };
 
-//console.log('se conectó')
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
 
+const fetchProductsOnce = async () => {
+  const querySnapshot = await getDocs(collection(db, 'productos'));
+  if (querySnapshot.empty) {
+    // Si la colección está vacía, agregamos los productos
+    productos.forEach((producto) => {
+      addDoc(collection(db, 'productos'), producto)
+        .then((docRef) => {
+          console.log(`Se agregó el documento con id: ${docRef.id}`);
+        })
+        .catch((error) => console.log(error));
+    });
+  } else {
+    console.log('Productos ya existen en la base de datos.');
+  }
+};
 
-// productos.forEach((producto) => {
-//  addDoc(collection(db, 'productos'), producto)
-//      .then((docRef) => {
-//          console.log(`se agregó el documento con id: ${docRef.id}`)
-//      })
-//      .catch((error) => console.log(error))
-//})
+const AppWithProducts = () => {
+  useEffect(() => {
+    fetchProductsOnce(); // Llamamos una vez al inicio
+  }, []); // El array vacío asegura que solo se ejecute una vez al montar
+
+  return <App />;
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AppWithProducts />
+  </React.StrictMode>
+);
